2917c3804ccebc6c5cc04d0784cca9d078fb6d25
Matheus Rodrigues <93332643+MatheusR0drigues@users.noreply.github.com>
2022-04-11 13:46:55 -0300

    Commiting files

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1>My first Data Analysis</h1>
+<h2>Instagram engagement by Categories</h2>
+<ul>
+  <li>This is my first Data Analysis to put in pratice some nice things that I learn recently</li>
+  <li>My main objective was to find the most engaged categories on Instagram</li>
+</ul>
+
+<h2>Conclusion</h2>
+  With this analysis I conclude the top 5 most engaged and relevant categories are:
+  <ol>
+    <li>Actors, Actresses & Cinema - 21.19%</li>
+    <li>Music - 20.19%</li>
+    <li>Lifestyle - 12.22%</li>
+    <li>Sports with a ball - 9.05%</li>
+    <li>Modeling - 6.19%</li>
+  </ol>
+  
+<h2>Info</h2>
+  Data from: https://www.kaggle.com/prasertk/top-1000-instagram-influencers
---
+++ webappdatas.py
@@ -0,0 +1,77 @@
+# importing the libaries
+
+import pandas as pd
+import streamlit as st
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+# Function to select the number of rows from the data base
+
+
+def show_number_rows(dataframe):
+
+    number_rows = st.sidebar.slider('Select the number of rows that'
+                                    'you want to show', min_value=1,
+                                    max_value=len(dataframe), step=1,
+                                    key=1)
+
+    return st.dataframe(df[:number_rows])
+
+
+def show_number_rows_df(dataframe):
+
+    number_rows = st.sidebar.slider('Select the number of rows that'
+                                    'you want to show', min_value=1,
+                                    max_value=len(dataframe), step=1,
+                                    key=2)
+    new_df = dynamic_df(dataframe, number_rows)
+    figure = plot_chart(new_df)
+
+    return st.pyplot(figure)
+
+# Function that create the chart
+
+
+def plot_chart(df):
+
+    sns.set_style('whitegrid')
+    fig, ax = plt.subplots(figsize=(13, 19))
+    ax = sns.barplot(x='Engagement percentagem', y='Category',
+                     data=df, palette='CMRmap')
+    ax.set_title('Engagement Percentage by Instagram Category'
+                 '(909 most engaged accounts analyzed)', fontsize=20)
+    ax.set_xlabel('Percentage (%)', fontsize=14)
+    ax.tick_params(rotation=20, axis='x')
+    ax.set_ylabel('Categories', fontsize=14)
+
+    return fig
+
+# Creating a dynamic data frame
+
+
+def dynamic_df(dataframe, rows):
+    df_v2 = dataframe[:rows]
+
+    return df_v2
+
+
+st.header('Data Analysis - Instagram Engagement')
+
+# Importing data base
+df = pd.read_csv('data/df_v4.csv')
+
+st.title('Instagram Categories')
+st.write('In this project we will analyze the most engaged'
+         ' categories on Instagram')
+
+# Table Filters
+option_1 = st.sidebar.checkbox('Show the Table')
+if option_1:
+    show_number_rows(df)
+
+# Ploting the chart
+
+option_2 = st.sidebar.checkbox('Dynamic Chart')
+
+if option_2:
+    show_number_rows_df(df)
